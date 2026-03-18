2cbfcc6d37976eec2990d572f1ed8eefdf941db4
gmkMasa <guillaume.majstruk@masagroup.net>
2026-03-18 10:23:58 +0100

    correction

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { ClickCountComponent } from './components/click-count.component';
+import { ClickCounterService } from './services/click-counter/click-counter.service';
 
 @Component({
   selector: 'app-root',
+  imports: [ClickCountComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {}
+export class App {
+  protected readonly canReset = computed(() => this.clickService.count() >= 10);
+
+  protected readonly clickService = inject(ClickCounterService);
+}
